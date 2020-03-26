333edced5880c2ef91c0ac70c872dd428e95ec2c
Nino Wili <nwili@ethz.ch>
2020-03-26 09:02:49 +0100

    updated plotting with deaths

--- nino_plot_n_fit_data.m
+++ nino_plot_n_fit_data.m
@@ -7,12 +7,12 @@ deaths = data_deaths(:,end);
 days = 1:size(data_cases,1);
 
 
-%% Fit exponential curve to 
-[xData, yData] = prepareCurveData( days, cases );
+%% Fit exponential curve to cases
+[xData, yData] = prepareCurveData( days, cases' );
 
 % Set up fittype and options.
 ft = fittype( 'a*2^(x/Td)' );
-excludedPoints = excludedata( xData, yData, 'Domain', [1 15] );
+excludedPoints = excludedata( xData, yData, 'Domain', [1 13] );
 opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
 opts.Display = 'Off';
 opts.StartPoint = [xData(1) 3];
@@ -31,5 +31,34 @@ plot( fitresult, xData, yData,'ok', excludedPoints,'xk');
 plot(  xData, CI,'k--' );
 xlabel Days
 ylabel Cases
+grid on
+title('Cases')
+legend off
+
+%% Fit exponential curve to deaths
+[xData, yData] = prepareCurveData( days, deaths' );
+
+% Set up fittype and options.
+ft = fittype( 'a*2^(x/Td)' );
+excludedPoints = excludedata( xData, yData, 'Range', [eps Inf] );
+opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
+opts.Display = 'Off';
+opts.StartPoint = [xData(1) 3];
+opts.Exclude = excludedPoints;
+
+% Fit model to data.
+[fitresult, gof] = fit( xData, yData, ft, opts );
+fitresult.Td
+CI = predint(fitresult,xData,0.95);
+
+% Plot fit with data.
+h=figure(2);
+clf
+hold on
+plot( fitresult, xData, yData,'ok', excludedPoints,'xk');
+plot(  xData, CI,'k--' );
+xlabel Days
+ylabel Deaths
+title('Deaths')
 grid on
 legend off

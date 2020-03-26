edd941a79c6a88cff3d45c495d4df0a8b5d5ca07
Nino Wili <nwili@ethz.ch>
2020-03-26 16:18:06 +0100

    minor changes again

--- nino_plot_n_fit_data.m
+++ nino_plot_n_fit_data.m
@@ -8,10 +8,10 @@ days = 1:size(data_cases,1);
 
 
 %% Fit exponential curve to cases
-[xData, yData] = prepareCurveData( days, cases' );
+[xData, yData] = prepareCurveData( days, (cases)' );
 
 % Set up fittype and options.
-ft = fittype( 'a*2^(x/Td)' );
+ft = fittype( '(a*2^(x/Td))' );
 excludedPoints = excludedata( xData, yData, 'Domain', [1 15] );
 opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
 opts.Display = 'Off';
@@ -36,29 +36,29 @@ grid on
 title('Cases')
 legend off
 
-%% Fit exponential curve to deaths
-[xData, yData] = prepareCurveData( days, deaths' );
-
-% Set up fittype and options.
-ft = fittype( 'a*2^(x/Td)' );
-excludedPoints = excludedata( xData, yData, 'Range', [eps Inf] );
-opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
-opts.Display = 'Off';
-opts.StartPoint = [xData(1) 3];
-opts.Exclude = excludedPoints;
-
-% Fit model to data.
-[fitresult, gof] = fit( xData, yData, ft, opts );
-fitresult.Td
-CI = predint(fitresult,xData,0.95);
-
-% Plot fit with data.
-subplot(2,1,2)
-hold on
-plot( fitresult, xData, yData,'ok', excludedPoints,'xk');
-plot(  xData, CI,'k--' );
-xlabel Days
-ylabel Deaths
-title('Deaths')
-grid on
-legend off
+% %% Fit exponential curve to deaths
+% [xData, yData] = prepareCurveData( days, deaths' );
+% 
+% % Set up fittype and options.
+% ft = fittype( 'a*2^(x/Td)' );
+% excludedPoints = excludedata( xData, yData, 'Range', [eps Inf] );
+% opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
+% opts.Display = 'Off';
+% opts.StartPoint = [xData(1) 3];
+% opts.Exclude = excludedPoints;
+% 
+% % Fit model to data.
+% [fitresult, gof] = fit( xData, yData, ft, opts );
+% fitresult.Td
+% CI = predint(fitresult,xData,0.95);
+% 
+% % Plot fit with data.
+% subplot(2,1,2)
+% hold on
+% plot( fitresult, xData, yData,'ok', excludedPoints,'xk');
+% plot(  xData, CI,'k--' );
+% xlabel Days
+% ylabel Deaths
+% title('Deaths')
+% grid on
+% legend off
